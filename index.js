eb91bbb052d5c67a70532164ba2eaecb45d04d80
Mae Bridgeman <62868541+maybe-maeb@users.noreply.github.com>
2025-09-09 10:20:02 -0600

    add alexa handler

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let http = require('http');
 
+//Alexa heading
+const skill = require('http://nodejs-redirect-2143773779.us-east-2.elb.amazonaws.com/'); // Path to your Alexa skill
+const handler = skill.handler;
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
