ce350cb9b274a2baf0531d7afe8c2aa90841047f
Mae Bridgeman <62868541+maybe-maeb@users.noreply.github.com>
2025-09-09 14:47:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -252,7 +252,6 @@ const skill = exports.handler = Alexa.SkillBuilders.custom()
     .addErrorHandlers(
         ErrorHandler)
     .withCustomUserAgent('sample/hello-world/v1.2')
-    .getSkill();
     
 console.log('skill:', skill);
 console.log('typeof skill:', typeof skill);
@@ -267,5 +266,3 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-exports.handler = skills.lambda();
