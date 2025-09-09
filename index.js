53b55e9c2991e1cde94363e34733dae947d2e26e
Mae Bridgeman <62868541+maybe-maeb@users.noreply.github.com>
2025-09-09 10:40:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//Make sure to run the following commands in your ec2 app:
+//npm install mysql2
+
 let http = require('http');
 
 const noun = require("./Model/noun.js");
@@ -6,3 +9,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
 }).listen(3000);
+
+console.log("Server running!");
