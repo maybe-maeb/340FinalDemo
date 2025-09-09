595d926cd335700cc63c0940feab49ac843853d7
Mae Bridgeman <62868541+maybe-maeb@users.noreply.github.com>
2025-09-09 10:31:44 -0600

    test new alexa header

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let http = require('http');
 
 //Alexa heading
-//const skill = require('http://nodejs-redirect-2143773779.us-east-2.elb.amazonaws.com/'); // Path to your Alexa skill
-//const handler = skill.handler;
+const skill = require('arn:aws:lambda:us-west-2:884178322167:function:f8d892d9-3fc6-43fc-9fc9-8eeb4682a06e:Release_0'); // Path to your Alexa skill
+const handler = skill.handler;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
