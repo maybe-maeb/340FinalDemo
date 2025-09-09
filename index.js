0d6c00f3b1fb35f0e893145430fd72d0ba3fa1c3
Mae Bridgeman <62868541+maybe-maeb@users.noreply.github.com>
2025-09-09 12:06:33 -0600

    test express

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ exports.handler = Alexa.SkillBuilders.custom()
 //Run Server
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World 2!')
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
