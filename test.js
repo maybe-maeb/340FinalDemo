4fa96545ca905e39d2011d02cd20660b3fe114cc
Mae Bridgeman <62868541+maybe-maeb@users.noreply.github.com>
2025-09-09 14:27:52 -0600

    Create test.js

---
+++ test.js
@@ -0,0 +1,21 @@
+const Alexa = require('ask-sdk-core');
+const { ExpressAdapter } = require('ask-sdk-express-adapter');
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder.speak('Hello world').getResponse();
+  }
+};
+
+const skill = Alexa.SkillBuilders.custom()
+  .addRequestHandlers(LaunchRequestHandler)
+  .lambda();
+
+console.log('appendAdditionalUserAgent:', skill.appendAdditionalUserAgent); // Should be a function
+
+const adapter = new ExpressAdapter(skill, false, false);
+
+console.log('ExpressAdapter created successfully');
