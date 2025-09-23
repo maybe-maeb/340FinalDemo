311079af1eae752a770c27369f2ec792624ad2eb
Mae Bridgeman <62868541+maybe-maeb@users.noreply.github.com>
2025-09-23 11:53:47 -0600

    Added debug statement to connection.js

--- Model/connection.js
+++ Model/connection.js
@@ -4,6 +4,7 @@ let connection = null;
 
 async function query(sql, params){
     if(null === connection){
+        console.log("Connecting to database...");
         connection = await mysql.createConnection({
             host: "student-databases.cvode4s4cwrc.us-west-2.rds.amazonaws.com",
             user: "MAE",
@@ -11,11 +12,18 @@ async function query(sql, params){
             database: 'MAE',
             connectTimeout: 3000,
         });
+        console.log("Connected to database.");
     }
     sql = mysql.format(sql, params);
     console.log(sql);
-    const [results] = await connection.execute(sql, params);
-    return results;
+    try {
+        const [results] = await connection.execute(sql, params);
+        return results;
+    }
+    catch (error){
+        console.error("Query failed: ", error);
+        throw error;
+    }
 }
 
 module.exports = {
